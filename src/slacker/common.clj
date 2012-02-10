17fbb77ed16483fa75a6473213f033ddbf8bb2cd
Daniel E. Renfer <duck@kronkltd.net>
2012-02-09 21:54:25 -0500

    mark dynamics

--- src/slacker/common.clj
+++ src/slacker/common.clj
@@ -1,11 +1,14 @@
 (ns slacker.common)
 
 (def
-  ^{:doc "Debug flag. This flag can be override by binding if you like to see some debug output."}
+  ^{:dynamic true
+    :doc "Debug flag. This flag can be override by binding if you like to see some debug output."}
   *debug* false)
 (def
-  ^{:doc "Timeout for synchronouse call."}
+  ^{:dynamic true
+    :doc "Timeout for synchronouse call."}
   *timeout* 10000)
 (def
-  ^{:doc "Protocol version."}
+  ^{:dynamic true
+    :doc "Protocol version."}
   version (short 4))
