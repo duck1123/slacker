01f5c033b80d148dcc2277272ecb4cbccb2786d8
Daniel E. Renfer <duck@kronkltd.net>
2012-02-09 22:12:47 -0500

    change groupid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.7.0-SNAPSHOT"
+(defproject net.kronkltd/slacker "0.7.0-SNAPSHOT"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [aleph "0.2.1-alpha1"]
